8c89d095388a716a48780cb1daf1f0480aa05f9d
LuanBertozzi7 <LuanBertozzi7@hotmail.com>
2025-12-21 23:08:05 -0400

    feat: process.on -> starting to handle errors

--- index.js
+++ index.js
@@ -4,6 +4,15 @@ import "dotenv/config";
 import { intents } from "./src/config/intents.js";
 import { client } from "./src/client.js";
 
+// error handling
+process.on("unhandledRejection", (reason, promise) => {
+  console.error(`Unhandled Rejection at: ${promise}, reason: ${reason}`);
+});
+
+process.on("uncaughtException", (error) => {
+  console.error("Uncaught Exception:", error);
+});
+
 client.once(Events.ClientReady, (readyClient) => {
   console.log(`${readyClient.user.tag}`);
 });
